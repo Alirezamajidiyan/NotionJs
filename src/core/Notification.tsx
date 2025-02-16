1947b5e16d75ee4f70a3152df929097c67bb4d54
Alirezamajidiyan <alirezadeveloperman@gmail.com>
2025-02-17 02:45:32 +0330

    updated

--- src/core/Notification.tsx
+++ src/core/Notification.tsx
@@ -55,18 +55,18 @@ const Notification: React.FC<NotificationProps> = ({
     switch (type) {
       case "success":
         return (
-          <svg className="icon" viewBox="0 0 24 24" aria-hidden="true">
+          <svg className="icon icon-success" viewBox="0 0 24 24" aria-hidden="true">
             <path
-              fill="currentColor"
+              fill="#4caf50"
               d="M9 16.2l-3.5-3.5L4 14.2l5 5 12-12-1.5-1.5z"
             />
           </svg>
         );
       case "error":
         return (
-          <svg className="icon" viewBox="0 0 24 24" aria-hidden="true">
+          <svg className="icon icon-error" viewBox="0 0 24 24" aria-hidden="true">
             <path
-              fill="currentColor"
+              fill="#f44336"
               d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 
                     10-4.48 10-10S17.52 2 12 2zm5 13l-1.41 
                     1.41L12 13.41l-3.59 3.59L7 15l3.59-3.59L7 
@@ -77,18 +77,18 @@ const Notification: React.FC<NotificationProps> = ({
         );
       case "warning":
         return (
-          <svg className="icon" viewBox="0 0 24 24" aria-hidden="true">
+          <svg className="icon icon-warning" viewBox="0 0 24 24" aria-hidden="true">
             <path
-              fill="currentColor"
+              fill="#ff9800"
               d="M1 21h22L12 2 1 21zm12-3h-2v-2h2v2zm0-4h-2v-4h2v4z"
             />
           </svg>
         );
       case "info":
         return (
-          <svg className="icon" viewBox="0 0 24 24" aria-hidden="true">
+          <svg className="icon icon-info" viewBox="0 0 24 24" aria-hidden="true">
             <path
-              fill="currentColor"
+              fill="#2196f3"
               d="M11 17h2v-6h-2v6zm0-8h2V7h-2v2zm1-7C6.48 
                     2 2 6.48 2 12s4.48 10 10 10 10-4.48 
                     10-10S17.52 2 12 2z"
@@ -97,9 +97,9 @@ const Notification: React.FC<NotificationProps> = ({
         );
       case "confirm":
         return (
-          <svg className="icon" viewBox="0 0 24 24" aria-hidden="true">
+          <svg className="icon icon-confrim" viewBox="0 0 24 24" aria-hidden="true">
             <path
-              fill="currentColor"
+              fill="#213358"
               d="M12 2C6.48 2 2 6.48 2 12s4.48 
                     10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 15h2v-2h-2v2zm0-4h2V7h-2v6z"
             />
